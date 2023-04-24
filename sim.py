41205561e8f612d54194afa4bf922d48fa29f75b
ActivePeter <1020401660@qq.com>
2023-04-24 13:29:25 +0800

    some data structures

---
+++ sim.py
@@ -0,0 +1,34 @@
+
+class SimEnv:
+    # node集合，node与node间的关系图
+
+    # 单次仿真的请求集合
+
+
+class Request: 
+    # 请求
+    # 按照一定的概率+基础函数生成
+    
+    # 会触发的第一个函数
+
+class Function:
+    # 函数
+
+    # cpu资源
+    # 内存资源
+    # 依赖的数据单元集合
+
+class DataBase:
+    # 数据库
+    # 数据集合
+
+class DataCell:
+    # 函数依赖的数据最小单元
+
+    # DataBase
+    
+class Node:
+    # 数据中心
+    # 节点一加入，生成与现有节点的通信速率，
+
+def sim_init_env():
