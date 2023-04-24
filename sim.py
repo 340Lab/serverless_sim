fe434f7671552d980100a641cda67c73e59ac0d5
ActivePeter <1020401660@qq.com>
2023-04-24 14:02:22 +0800

    s

--- sim.py
+++ sim.py
@@ -4,7 +4,9 @@ class SimEnv:
     nodes
     # 单位时间段
     periods
-
+    # 节点间网速图
+    node2node_graph
+    
 class SimPeriod:
     # 用于记录整体的负载情况，成本情况
 
@@ -29,8 +31,14 @@ class Function:
     datacells
     # 访问次数
     datacells_visit_time
+    
+
+class FunctionInstance:
+
     # 运行在的node
     node
+    # 函数
+    fn
 
 class DataBase:
     # 数据库
@@ -48,6 +56,8 @@ class DataCell:
     # 数据大小
     data_size
 
+class ServerlessController:
+    # 分发请求，调度，扩缩容
 
 class Node:
     # 数据中心
@@ -58,6 +68,9 @@ class Node:
 
     #函数容器
     functions
+
+    #serverless总控节点
+    serverless_controller
 
 class Node2NodeGraph:
     # 节点到节点的网速
