881290870e04b659961a6665fbfa0ca2d9c25ace
zxy-ywsy <392597400@qq.com>
2024-04-08 12:32:53 +0800

    feat: complete the basic network_topo
    topo2 debug
    
    topo2 fix
    
    topo2 fix
    
    topo2 fix2
    
    2
    
    cancel remove mouse event for svg
    
    3
    
    4
    
    fix

--- serverless_sim_ui/src/network_topo.ts
+++ serverless_sim_ui/src/network_topo.ts
@@ -1,12 +1,23 @@
-export class UINode{
+export class UINode {
     constructor(
-        public x:number,
-        public y:number,
-        public zIndex:number,
-        public id:number) {
-    }
+        public x: number,
+        public y: number,
+        public zIndex: number,
+        public id: number,
+        public index: number
+    ) { }
 }
 
-export class Topo{
-    nodes: Node[]
+export class UILink {
+    constructor(
+        public source: [number, number],
+        public bandwidth: number
+    ) { }
+}
+
+export class Topo {
+    constructor(
+        public nodes: UINode[],
+        public links: Map<string, UILink>
+    ) { }
 }
